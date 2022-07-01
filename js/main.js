8340d65e75a07250c281b89c632b6bfb9a70cd81
OlgaDormio2 <dormioo67@gmail.com>
2022-07-01 14:01:50 +0300

    8.19. Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createPhotos} from './data.js';
 import {createHtmlImages} from './images.js';
+import {createFormEdit} from  './formEdit.js';
 
 const photos = createPhotos();
 createHtmlImages(photos);
-
+createFormEdit();
